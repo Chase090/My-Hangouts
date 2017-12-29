2ff6633c2dc530c09cebc1c079718c17c4d2b91a
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-29 12:08:53 -0500

    fixed .hangouts. User show page in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   has_many :friendships, dependent: :destroy  #if user is destroyed, so is friendship(s)
   has_many :friends, through: :friendships
 
-  has_many :myhangouts, class_name: "Hangout", foreign_key: "owner_id", dependent: :destroy
+  has_many :invitations, foreign_key: "guest_id", dependent: :destroy
 
-  has_many :invitations
   has_many :hangouts, through: :invitations
+
+  has_many :myhangouts, class_name: "Hangout", foreign_key: "owner_id", dependent: :destroy
 
   has_secure_password
 
@@ -21,14 +22,21 @@ class User < ApplicationRecord
   end
 
   def hangouts_count
-    hangouts.count
-  end
-
-  def invitations_count
     hangouts.select do |h|
       h.confirmed == true
     end.count
   end
+
+  def invitations_count
+    hangouts.select do |h|
+      h.confirmed == false
+    end.count
+  end
+
+  def friends_count
+    friends.count
+  end
+
 
 
 end
